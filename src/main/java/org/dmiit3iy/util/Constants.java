38a3c35f857895bc517560367434c4440f4c3485
Dmiit3iy <94470190+Dmiit3iy@users.noreply.github.com>
2023-08-27 22:49:18 +0500

    Update Constants.java

--- src/main/java/org/dmiit3iy/util/Constants.java
+++ src/main/java/org/dmiit3iy/util/Constants.java
@@ -3,6 +3,6 @@ package org.dmiit3iy.util;
 public class Constants {
     //Введите свой email
     public static final String EMAIL ="your_email@mail.ru";
-    //Введите свой пароль для
+    //Введите свой пароль для внешних приложений
     public static final String EMAIL_PASSWORD ="YoUrPasSwOrD";
 }
